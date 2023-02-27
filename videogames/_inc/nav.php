d24df728acef7a47835b01d6ca5b9a8ef95d3cd2
H3Airness <damienraunier@gmail.com>
2023-02-27 16:49:32 +0100

    v1.1

---
+++ videogames/_inc/nav.php
@@ -0,0 +1,6 @@
+<nav>
+    <img src="/img/logo.jpg" href="/index.php">
+
+    <a href="/index.php">Acceuil</a>
+
+</nav>
---
+++ videogames/index.php
@@ -0,0 +1,12 @@
+<?php
+    //inclusions
+    require_once '_inc/header.php';
+    require_once '_inc/nav.php';
+?>
+
+    <h1>Acceuil</h1>
+
+<?php
+    //inclusions
+    require_once '_inc/footer.php';
+?>
